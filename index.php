becd5caaf00ea7210a133cdb61ee0d5c714428e8
Debfront <deborah_monteiro12@hotmail.com>
2020-11-14 21:37:54 -0300

    14/11
    Formulario cadastro

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style type="text/css">
+        #tamanhocontainer{
+            width:500px;
+        }
+    </style>
+</head>
+<body>
+<div class="container" id="tamanhocontainer" style="margin-top:40px">
+    <form>
+       <div class="form-group">
+        <label>Frota</label>
+        <input type="number" class="form-control" placeholder=" Digite o número da frota">
+      </div>
+      <div class="form-group">
+        <label>Cliente</label>
+        <input type="text" class="form-control" placeholder=" Digite o nome do cliente">
+      </div>
+      
+      <div class="form-group">
+        <label>Nota fiscal</label>
+        <input type="number" class="form-control" placeholder="Insira o número da nota fiscal">
+      </div>
+      
+      <div class="form-group">
+        <label>Transportadora</label>
+        <input type="text" class="form-control" placeholder="Insira o nome da transportadora">
+      </div>
+      <div class="form-group">
+        <label>Data</label>
+        <input type="date" value="2017-09-13">
+      </div>
+      <button type="submit" class="btn btn-success btn-sm"> Cadastrar</button>
+    </form>   
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
